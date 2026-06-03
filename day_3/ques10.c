86b8893889112b525e8d8b0b5ba2628ea824cc74
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-03 17:13:03 +0530

    Create ques10.c

---
+++ day_3/ques10.c
@@ -0,0 +1,24 @@
+//WAP TO PRINT PRIME NUMBERS IN A RANGE
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,flag=0;
+    printf("enter the starting number of range:");
+    scanf("%d",&a);
+    printf("enter the ending number of range:");
+    scanf("%d",&b);
+    for(i=a;i<=b;i++)
+    {
+      flag=0;
+      for(j=2;j<i;j++)
+    {
+      if(i%j==0)
+    {
+      flag=1;
+      break;
+    }
+    }
+      if(flag==0)
+      printf("%d",i);
+    }
+}
