d9e88fda4e7073f1537f5b009de35883200e788f
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-06 18:25:33 +0530

    Create ques21.c

---
+++ day_6/ques21.c
@@ -0,0 +1,19 @@
+//WAP TO CONVERT DECIMAL TO BINARY
+#include<stdio.h>
+int main()
+{int n,r,ans=0,contri=1;
+printf("enter the decimal number");
+scanf("%d",&n);
+while(n>0)
+{
+ r=n%2;
+ if(r==1)
+{
+    ans=ans+contri;
+}
+ n=n/2;
+ contri=contri*10;
+}
+printf("%d",ans);
+return 0;
+}
