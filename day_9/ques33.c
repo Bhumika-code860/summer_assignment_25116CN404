16da612692b411c01aa229ac6c7e0e093fb53eb7
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-09 11:14:40 +0530

    Create ques33.c

---
+++ day_9/ques33.c
@@ -0,0 +1,15 @@
+//WAP TO PRINT REVERSE STAR PATTERN
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=0;j<=5-i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
