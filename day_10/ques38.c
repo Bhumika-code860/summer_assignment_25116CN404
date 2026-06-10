fbd25ae13d7e0de14d13172453bf4f4e820a1476
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-10 11:09:52 +0530

    Create ques38.c

---
+++ day_10/ques38.c
@@ -0,0 +1,22 @@
+//WAP TO PRINT REVERSE PYRAMID
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(k=i;k<=5;k++)
+        {
+            printf("*");
+        }
+        for(k=i;j<5;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
