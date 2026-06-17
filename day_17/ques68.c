e24bbd3ad8748dc415c689388bc4ec98c60aa5cd
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-17 17:57:06 +0530

    Create ques68.c

---
+++ day_17/ques68.c
@@ -0,0 +1,35 @@
+//WAP TO FIND COMMON ELEMENTS
+#include<stdio.h>
+int main()
+{
+    int n,i,j,arr[50],brr[50],x;
+    printf("Enter the size of first array:");
+    scanf("%d",&n);
+    printf("Enter the size of second array:");
+    scanf("%d",&x);
+    printf("enter elements for first array:");
+    for(i=0;i<n;i++)
+    {
+        printf("enter arr[%d]:",i);
+        scanf("%d",&arr[i]);
+    }
+    printf("enter elements for second array:");
+    for(i=0;i<x;i++)
+    {
+        printf("enter arr[%d]:",i);
+        scanf("%d",&brr[i]);
+    }
+    printf("Common elements in both the arrays are:");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<x;j++)
+        {
+            if(arr[i]==brr[j])
+            {
+                printf("%d\t",arr[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
