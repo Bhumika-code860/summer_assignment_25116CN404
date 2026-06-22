003e99732b8b406d9fd20b551db89c0134bf3437
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-22 19:00:41 +0530

    Create ques87.c

---
+++ day_22/ques87.c
@@ -0,0 +1,26 @@
+//WAP TO CHARACTER FREQUENCY
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,count=0;
+    char s[50];
+    printf("enter a sentence:");
+    gets(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+        count=1;
+        if(s[i]=='*')
+            continue;
+        for(j=i+1;s[j]!='\0';j++)
+        {
+            if(s[i]==s[j])
+            {
+                count++;
+                s[j]='*';
+            }
+        }
+    printf("%c=%d\n",s[i],count);
+    }
+    return 0;
+}
