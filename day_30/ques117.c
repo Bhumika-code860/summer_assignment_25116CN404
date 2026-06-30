1f284017500ade6f69515c6c9060cd79bad90e24
Bhumika-code860 <bhumibansal785@gmail.com>
2026-06-30 11:37:59 +0530

    Create ques117.c

---
+++ day_30/ques117.c
@@ -0,0 +1,34 @@
+//WAP TO CREATE STUDENT RECORD SYSTEMSS USING ARRAYS AND STRINGS
+#include<stdio.h>
+#include<string.h>
+struct student
+{
+    int roll;
+    char name[50];
+    float marks;
+};
+int main()
+{
+    int i,n;
+    printf("ENTER NUMBER OF STUDENTS:");
+    scanf("%d",&n);
+    struct student s[50];
+    for(i=0;i<n;i++)
+    {
+        printf("\nSTUDENT %d\n",i+1);
+        printf("ROLL NO:");
+        scanf("%d",&s[i].roll);
+        printf("NAME:");
+        scanf("%s",s[i].name);
+        printf("MARKS:");
+        scanf("%f",&s[i].marks);
+    }
+    printf("\n-----STUDENT RECORDS----\n");
+    for(i=0;i<n;i++)
+    {
+        printf("ROLL NO:%d",s[i].roll);
+        printf("NAME:%s",s[i].name);
+        printf("MARKS:%f",&s[i].marks);
+    }
+    return 0;  
+}
